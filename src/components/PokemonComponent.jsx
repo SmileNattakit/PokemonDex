80b7796b7d5285823e553ace6a49cb67333e5368
SmileNattakit <nattakit.rattanakeha@gmail.com>
2024-06-10 01:10:01 +0700

    add search function

--- src/components/PokemonComponent.jsx
+++ src/components/PokemonComponent.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
+import { useForm } from 'react-hook-form';
+import { useState, useEffect } from 'react';
+
 const typeColors = {
   grass: 'bg-green-500',
   fire: 'bg-red-500',
@@ -22,6 +25,10 @@ const typeColors = {
 };
 
 const PokemonComponent = () => {
+  const { register, handleSubmit } = useForm();
+  const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+  const [none, setNone] = useState(false);
+
   const {
     isLoading,
     error,
@@ -33,7 +40,6 @@ const PokemonComponent = () => {
         'https://pokeapi.co/api/v2/pokemon?limit=151'
       );
       const pokemonUrls = response.data.results.map((pokemon) => pokemon.url);
-
       const pokemonResponses = await Promise.all(
         pokemonUrls.map((url) => axios.get(url))
       );
@@ -45,60 +51,97 @@ const PokemonComponent = () => {
     }
   );
 
+  useEffect(() => {
+    if (pokemonList && pokemonList.length > 0) {
+      onSubmit({ search: '' });
+    }
+  }, [pokemonList]);
+
+  const onSubmit = (data) => {
+    const filtered = pokemonList.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(data.search.toLowerCase())
+    );
+    setFilteredPokemonList(filtered);
+
+    const hasMatchingPokemon = filtered.length > 0;
+    setNone(!hasMatchingPokemon);
+  };
+
   if (isLoading) {
-    return <div>กำลังโหลด...</div>;
+    return (
+      <div className="w-[100vw] h-[40vw]  flex justify-center items-center">
+        <span className="loading loading-spinner loading-lg  text-2xl flex justify-center items-center"></span>
+      </div>
+    );
   }
 
   if (error) {
     return <div>เกิดข้อผิดพลาด: {error.message}</div>;
   }
 
+  const PokemonCard = ({ pokemon, index }) => (
+    <li key={pokemon.name} className="m-4">
+      <div className="card card-compact w-50 bg-base-100 shadow-xl">
+        <p className="text-xl font-semibold px-4">
+          #{String(index + 1).padStart(3, '0')}
+        </p>
+        <figure>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{pokemon.name}</h2>
+          <div>
+            {pokemon.types.map((type) => (
+              <span
+                key={type.type.name}
+                className={`badge ${
+                  typeColors[type.type.name] || 'bg-gray-500'
+                }`}
+              >
+                {type.type.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+
   return (
     <div>
-      <label className="input input-bordered flex items-center gap-2 my-5 mx-4 ">
-        <input type="text" className="grow " placeholder="Search" />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-            clipRule="evenodd"
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label className="input input-bordered flex items-center gap-2 my-5 mx-4 ">
+          <input
+            type="text"
+            className="grow "
+            placeholder="Search"
+            {...register('search')}
           />
-        </svg>
-      </label>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      </form>
+
       <ul className="grid grid-cols-6">
-        {pokemonList.map((pokemon, index) => (
-          <li key={pokemon.name} className="m-4">
-            <div className="card card-compact w-50 bg-base-100 shadow-xl">
-              <p className="text-xl font-semibold px-4">
-                #{String(index + 1).padStart(3, '0')}{' '}
-                {/* ปรับปรุงการแสดงผลเลข index */}
-              </p>
-              <figure>
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">{pokemon.name}</h2>
-                <div>
-                  {pokemon.types.map((type) => (
-                    <span
-                      key={type.type.name}
-                      className={`badge ${
-                        typeColors[type.type.name] || 'bg-gray-500'
-                      }`}
-                    >
-                      {type.type.name}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </li>
-        ))}
+        {filteredPokemonList.length > 0 ? (
+          filteredPokemonList.map((pokemon, index) => (
+            <PokemonCard key={pokemon.name} pokemon={pokemon} index={index} />
+          ))
+        ) : none ? (
+          <div className="text-center mt-4 w-[100vw] h-[40vw] text-2xl flex justify-center items-center">
+            ไม่พบข้อมูล Pokemon ที่ตรงกัน
+          </div>
+        ) : null}
       </ul>
     </div>
   );
